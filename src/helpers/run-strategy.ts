dbc9422ccf760cb3a3e57e8528fa28830f08d208
Davide Morra <davide.morra@volcanicminds.com>
2024-10-24 12:45:59 +0200

    remove comments

--- src/helpers/run-strategy.ts
+++ src/helpers/run-strategy.ts
@@ -29,16 +29,13 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
 
   const strategyFilePath = getStrategy(runParams.strategyName, runParams.rootPath)
 
-  // Validate strategy file exists
   if (!strategyFilePath) {
     throw new BacktestError(`Strategy file ${runParams.strategyName}.ts not found.`, ErrorCode.StrategyNotFound)
   }
 
-  // Import strategy
   delete require.cache[require.resolve(strategyFilePath)]
   const strategy = await import(strategyFilePath)
 
-  // Validate strategy function exists
   if (strategy?.runStrategy === undefined) {
     throw new BacktestError(
       `${runParams.strategyName} file does not have a function with the name of runStrategy.\nIt is mandatory to export a function with this name:\n\nexport async function runStrategy(bth: BTH) {}`,
@@ -46,11 +43,9 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
     )
   }
 
-  // Define error handler
   let returnAnError: boolean = false
   let returnError = { error: false, data: '' }
 
-  // Find if need to run multi and get there permutations
   let multiSymbol = runParams.historicalMetaData.length > 1
   let multiValue = false
   let permutations = [{}]
@@ -67,7 +62,6 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
   }
 
   for (let symbolCount = 0; symbolCount < runParams.historicalMetaData.length; symbolCount++) {
-    // Get candles
     const candlesRequest = await getCandles(runParams.historicalMetaData[symbolCount])
     if (!candlesRequest) {
       throw new BacktestError(`Candles for ${runParams.historicalMetaData[symbolCount]} not found`, ErrorCode.NotFound)
@@ -83,7 +77,6 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
       if (multiValue) runParams.params = permutations[permutationCount]
 
       if ((multiValue || multiSymbol) && !historicalMetaData) {
-        // Get candle metaData
         historicalMetaData = await getCandleMetaData(runParams.historicalMetaData[symbolCount])
         if (!historicalMetaData) {
           throw new BacktestError(
@@ -98,7 +91,6 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
         }
       }
 
-      // Create / clear order book and all orders
       orderBook.bought = false
       orderBook.boughtLong = false
       orderBook.boughtShort = false
@@ -111,17 +103,14 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
       orderBook.takeProfit = 0
       await clearOrders()
 
-      // Define all current worths
       const allWorths: Worth[] = []
 
-      // Find where loop should start and end
       let candleIndexes = await findCandleIndex(candles, runParams.startTime, runParams.endTime)
       let candleIndex = candleIndexes.startIndex
       const candleIndexEnd = candleIndexes.endIndex
       let allCandles = candles.slice(candleIndex, candleIndexEnd)
       numberOfCandles = candleIndexEnd - candleIndex
 
-      // Define strategy run metaData
       const runMetaData = {
         highestAmount: runParams.startingAmount,
         highestAmountDate: candles[candleIndex].closeTime,
@@ -144,27 +133,18 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
         sharpeRatio: 0
       }
 
-      // ----------------------------------------------------
-      // |                  START FOR LOOP                  |
-      // ----------------------------------------------------
       for (candleIndex; candleIndex < candleIndexEnd; candleIndex++) {
-        // Define current candle
         const currentCandle = candles[candleIndex]
 
-        // Define if can buy or sell based on getCandles decision
         let canBuySell = true
 
-        // Buy function that calls the real one
         async function buy(buyParams?: BuySell) {
-          // Dont allow buy if highest needed candle is not met
           if (!canBuySell) {
             logger.trace('Buy blocked until highest needed candles are met')
           } else {
-            // Define buy price if needed
             if (buyParams === undefined) buyParams = {}
             if (buyParams.price === undefined) buyParams.price = currentCandle.close
 
-            // Create the real buy params
             const buyParamsReal = {
               currentClose: currentCandle.close,
               percentFee: runParams.percentFee,
@@ -173,7 +153,6 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
               ...buyParams
             }
 
-            // Check stop loss is not set with long and short
             if (
               buyParams.stopLoss !== undefined &&
               buyParams.stopLoss > 0 &&
@@ -184,7 +163,6 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
               returnError = { error: true, data: 'Cannot define a stop loss if in a long and a short' }
             }
 
-            // Check take profit is not set with long and short
             if (
               buyParams.takeProfit !== undefined &&
               buyParams.takeProfit > 0 &&
@@ -195,32 +173,25 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
               returnError = { error: true, data: 'Cannot define a take profit if in a long and a short' }
             }
 
-            // Set stop loss and take profit
             if (buyParams.stopLoss !== undefined && buyParams.stopLoss > 0) orderBook.stopLoss = buyParams.stopLoss
             if (buyParams.takeProfit !== undefined && buyParams.takeProfit > 0)
               orderBook.takeProfit = buyParams.takeProfit
 
-            // Perform the buy
             const buyResults = await realBuy(buyParamsReal)
 
-            // Handle if buying error
             if (buyResults) {
               logger.trace(`Real buy performed`)
             }
           }
         }
 
-        // Sell function that call the real one
         async function sell(sellParams?: BuySell) {
-          // Dont allow sell if highest needed candle is not met
           if (!canBuySell) {
             logger.trace('Sell blocked until highest needed candles are met')
           } else {
-            // Define sell price if needed
             if (sellParams === undefined) sellParams = {}
             if (sellParams.price === undefined) sellParams.price = currentCandle.close
 
-            // Create the real sell params
             const sellParamsReal = {
               currentClose: currentCandle.close,
               percentFee: runParams.percentFee,
@@ -229,73 +200,52 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
               ...sellParams
             }
 
-            // Perform the sell
             const sellResults = await realSell(sellParamsReal)
 
-            // Handle if selling error
             if (sellResults) {
               logger.trace(`Real sell performed`)
             }
           }
         }
 
-        // Get candle data
         async function getCandles(type: keyof Candle | 'all', start: number, end?: number) {
-          // Check if candle start time is valid
           const candleCheck = end === undefined ? start : end
 
-          // If highest needed candles is not met, return 0 or array of 0's and disable buy and sells
           if (candleIndex - candleCheck < 0) {
             canBuySell = false
             return end === undefined ? 0 : new Array(end - start).fill(0)
           }
 
-          // Allow buy and sells if set to false previously
           canBuySell = true
 
-          // Return a single value
           if (end === undefined)
             return type === 'all' ? candles[candleIndex - start] : candles[candleIndex - start][type]
 
-          // Return all properties of each candle in the range when type is all
           if (type === 'all') return candles.slice(candleIndex - end, candleIndex - start)
 
-          // Return a specific type of each candle in the range
           return candles.slice(candleIndex - end, candleIndex - start).map((candle) => candle[type])
         }
 
-        // Return an error before running if needed
         if (returnAnError) {
           throw new BacktestError(returnError.data as string, ErrorCode.ActionFailed)
         }
 
-        // Check stop loss
         if (orderBook.stopLoss > 0) {
-          // Long stop loss check
           if (orderBook.baseAmount > 0) {
             if (currentCandle.low <= orderBook.stopLoss) await sell({ price: orderBook.stopLoss })
-          }
-
-          // Short stop loss check
-          else if (orderBook.borrowedBaseAmount > 0) {
+          } else if (orderBook.borrowedBaseAmount > 0) {
             if (currentCandle.high >= orderBook.stopLoss) await sell({ price: orderBook.stopLoss })
           }
         }
 
-        // Check take profit
         if (orderBook.takeProfit > 0) {
-          // Long take profit check
           if (orderBook.baseAmount > 0) {
             if (currentCandle.high >= orderBook.takeProfit) await sell({ price: orderBook.takeProfit })
-          }
-
-          // Short take profit check
-          else if (orderBook.borrowedBaseAmount > 0) {
+          } else if (orderBook.borrowedBaseAmount > 0) {
             if (currentCandle.low <= orderBook.takeProfit) await sell({ price: orderBook.takeProfit })
           }
         }
 
-        // Get current worth and check if error
         const worth = await getCurrentWorth(
           currentCandle.close,
           currentCandle.high,
@@ -312,7 +262,6 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
           )
         }
 
-        // Push in worth to all worths
         allWorths.push({
           close: worth.close,
           high: worth.high,
@@ -321,7 +270,6 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
           time: currentCandle.closeTime
         })
 
-        // Update asset amount
         if (currentCandle.high > runMetaData.highestAssetAmount) {
           runMetaData.highestAssetAmount = currentCandle.high
           runMetaData.highestAssetAmountDate = currentCandle.closeTime
@@ -331,17 +279,14 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
           runMetaData.lowestAssetAmountDate = currentCandle.closeTime
         }
 
-        // Update metaData worths and drawdowns
         if (worth.high > runMetaData.highestAmount) {
           runMetaData.highestAmount = worth.high
           runMetaData.highestAmountDate = currentCandle.closeTime
         }
         if (worth.low < runMetaData.lowestAmount) {
-          // Update lowest worth
           runMetaData.lowestAmount = worth.low
           runMetaData.lowestAmountDate = currentCandle.closeTime
 
-          // Update max drawdown amount
           if (runMetaData.highestAmount - worth.low > runMetaData.maxDrawdownAmount) {
             runMetaData.maxDrawdownAmount = round(runMetaData.highestAmount - worth.low)
             runMetaData.maxDrawdownAmountDates = `${new Date(
@@ -352,7 +297,6 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
             )}`
           }
 
-          // Update max drawdown percent
           const drawdownPercent = ((runMetaData.highestAmount - worth.low) / runMetaData.highestAmount) * 100
           if (drawdownPercent > runMetaData.maxDrawdownPercent) {
             runMetaData.maxDrawdownPercent = round(drawdownPercent)
@@ -365,7 +309,6 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
           }
         }
 
-        // Call users strategy
         try {
           await strategy.runStrategy({
             currentCandle,
@@ -380,23 +323,18 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
           throw new BacktestError(`Ran into an error running the strategy with error ${error}`, ErrorCode.StrategyError)
         }
 
-        // Return an error after running if needed
         if (returnAnError) {
           throw new BacktestError(returnError.data as string, ErrorCode.ActionFailed)
         }
 
-        // Update number of candles invested
         if (orderBook.bought) runMetaData.numberOfCandlesInvested++
 
-        // Sell if bought into and on last candle
         if (candleIndex === candleIndexEnd - 1 && orderBook.bought) await sell()
       }
 
-      // Get the sharpe ratio
       runMetaData.sharpeRatio = calculateSharpeRatio(allWorths)
 
       if (multiValue || multiSymbol) {
-        // Update asset amounts
         assetAmounts.startingAssetAmount = runMetaData.startingAssetAmount
         assetAmounts.endingAssetAmount = runMetaData.endingAssetAmount
         assetAmounts.highestAssetAmount = runMetaData.highestAssetAmount
@@ -405,7 +343,6 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
         assetAmounts.lowestAssetAmountDate = runMetaData.lowestAssetAmountDate
         assetAmounts.numberOfCandles = numberOfCandles
 
-        // Push in relevant return data if multi value
         if (historicalMetaData) {
           permutationReturn.push({
             ...runParams.params,
@@ -420,12 +357,10 @@ export async function run(runParams: RunStrategy): Promise<RunStrategyResult | R
           })
         }
       } else {
-        // Return the runs data
         return { allOrders, runMetaData, allWorths, allCandles } as RunStrategyResult
       }
     }
   }
 
-  // Return the multi value data
   return permutationReturn
 }
